f8f434721d94035efbdc85b7c977a6b9d86063c9
Rachit Gaur <rachitgaur2k24@gmail.com>
2026-07-12 22:32:27 +0530

    Fix config for Render deployment

--- backend/config/config.js
+++ backend/config/config.js
@@ -8,9 +8,6 @@ if (!process.env.MONGO_URI) {
 if (!process.env.JWT_SECRET) {
   throw new Error("JWT_SECRET is not defined in env file");
 }
-if (!process.env.PORT) {
-  throw new Error("PORT is not defined in env file");
-}
 if (!process.env.RAZORPAY_KEY_ID) {
   throw new Error("RAZORPAY_KEY_ID is not defined in env file");
 }
@@ -32,12 +29,15 @@ if (!process.env.CLOUDINARY_API_SECRET) {
 const config = {
   MONGO_URI: process.env.MONGO_URI,
   JWT_SECRET: process.env.JWT_SECRET,
-  PORT: process.env.PORT,
+  PORT: process.env.PORT || 5000,
   RAZORPAY_KEY_ID: process.env.RAZORPAY_KEY_ID,
   RAZORPAY_KEY_SECRET: process.env.RAZORPAY_KEY_SECRET,
   CLOUDINARY_CLOUD_NAME: process.env.CLOUDINARY_CLOUD_NAME,
   CLOUDINARY_API_KEY: process.env.CLOUDINARY_API_KEY,
- CLOUDINARY_API_SECRET: process.env.CLOUDINARY_API_SECRET
+  CLOUDINARY_API_SECRET: process.env.CLOUDINARY_API_SECRET,
+  EMAIL_USER: process.env.EMAIL_USER,
+  EMAIL_PASS: process.env.EMAIL_PASS,
+  RAZORPAY_WEBHOOK_SECRET: process.env.RAZORPAY_WEBHOOK_SECRET,
 };
 
 export default config;
